62866b6b359e21d9e8202823c11fc812794b2c17
Stanislav <sl.stas1@mail.ru>
2021-04-03 23:35:07 +0300

    Initial Commit

---
+++ Project/Context/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=project;Username=postgres;Password=staslichagin");
+        }
+    }
+}
---
+++ Project/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Project.Context;
+using Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        ApplicationContext applicationContext = new ApplicationContext();
+
+        public ActionResult Index()
+        {
+            IEnumerable<User> users = applicationContext.Users;
+            ViewBag.Users = users;
+            return View();
+        }
+    }
+}
