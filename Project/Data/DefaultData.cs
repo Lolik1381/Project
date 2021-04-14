6aed7f8bf807b0b912fe8003ead1a47a1159f256
Yulia Radzhabova <itigrenok2001@gmail.com>
2021-04-14 23:24:42 +0300

    AP-8 - заполнение БД (Landmarks)

--- Project/Data/DefaultData.cs
+++ Project/Data/DefaultData.cs
@@ -57,6 +57,157 @@ namespace Project.Data
             dataBase.SaveChanges();
         }
 
+        public void createLandmark()
+        {
+            Models.Photo photo1 = new Models.Photo { image = getByteImage(@"wwwroot\img\Upper_Geyser_Basin.jpg"), name = @"img\Upper_Geyser_Basin.jpg" };
+            Models.Photo photo2 = new Models.Photo { image = getByteImage(@"wwwroot\img\Upper_Geyser_Basin_1.jpg"), name = @"img\Upper_Geyser_Basin_1.jpg" };
+            Models.Photo photo3 = new Models.Photo { image = getByteImage(@"wwwroot\img\big_prisma.jpg"), name = @"img\big_prisma.jpg" };
+            Models.Photo photo4 = new Models.Photo { image = getByteImage(@"wwwroot\img\azul.jpg"), name = @"img\azul.jpg" };
+            Models.Photo photo5 = new Models.Photo { image = getByteImage(@"wwwroot\img\azul_1.jpg"), name = @"img\azul_1.jpg" };
+            Models.Photo photo6 = new Models.Photo { image = getByteImage(@"wwwroot\img\hana.jpg"), name = @"img\hana.jpg" };
+            Models.Photo photo7 = new Models.Photo { image = getByteImage(@"wwwroot\img\magic-kingdom.jpg"), name = @"img\magic-kingdom.jpg" };
+            Models.Photo photo8 = new Models.Photo { image = getByteImage(@"wwwroot\img\universal.jpg"), name = @"img\universal.jpg" };
+            Models.Photo photo9 = new Models.Photo { image = getByteImage(@"wwwroot\img\universal_1.jpg"), name = @"img\universal_1.jpg" };
+            Models.Photo photo10 = new Models.Photo { image = getByteImage(@"wwwroot\img\soldiers-pass.jpg"), name = @"img\soldiers-pass.jpg" };
+            Models.Photo photo11 = new Models.Photo { image = getByteImage(@"wwwroot\img\bridge-trail.jpg"), name = @"img\punta-kana.jpg" };
+            Models.Photo photo12 = new Models.Photo { image = getByteImage(@"wwwroot\img\maya-museum.jpg"), name = @"img\maya-museum.jpg" };
+            Models.Photo photo13 = new Models.Photo { image = getByteImage(@"wwwroot\img\El-Rey.jpg"), name = @"img\El-Rey.jpg" };
+            Models.Photo photo14 = new Models.Photo { image = getByteImage(@"wwwroot\img\El-Rey_1.jpg"), name = @"img\El-Rey_1.jpg" };
+            Models.Photo photo15 = new Models.Photo { image = getByteImage(@"wwwroot\img\playa-delfines.jpg"), name = @"img\playa-delfines.jpg" };
+            Models.Photo photo16 = new Models.Photo { image = getByteImage(@"wwwroot\img\empire-state-building.jpg"), name = @"img\empire-state-building.jpg" };
+            Models.Photo photo17 = new Models.Photo { image = getByteImage(@"wwwroot\img\Brooklyn-bridge.jpg"), name = @"img\Brooklyn-bridge.jpg" };
+            Models.Photo photo18 = new Models.Photo { image = getByteImage(@"wwwroot\img\central-park.jpg"), name = @"img\central-park.jpg" };
+            Models.Photo photo19 = new Models.Photo { image = getByteImage(@"wwwroot\img\Strip.jpg"), name = @"img\Strip.jpg" };
+            Models.Photo photo20 = new Models.Photo { image = getByteImage(@"wwwroot\img\Nevada_garden.jpg"), name = @"img\Nevada_garden.jpg" };
+            Models.Photo photo21 = new Models.Photo { image = getByteImage(@"wwwroot\img\Nevada_garden_1.jpg"), name = @"img\Nevada_garden_1.jpg" };
+            Models.Photo photo22 = new Models.Photo { image = getByteImage(@"wwwroot\img\tower-of-London.jpg"), name = @"img\tower-of-London.jpg" };
+            Models.Photo photo23 = new Models.Photo { image = getByteImage(@"wwwroot\img\tower-of-London_1.jpg"), name = @"img\tower-of-London_1.jpg" };
+            Models.Photo photo24 = new Models.Photo { image = getByteImage(@"wwwroot\img\British-museum.jpg"), name = @"img\British-museum.jpg" };
+            dataBase.photos.AddRange(photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8, photo9, photo10, photo11, photo12, photo13, photo14, photo15, photo16, photo17, photo18, photo19, photo20, photo21, photo22, photo23, photo24);
+
+            //direction1 - "Йеллоустонский национальный парк, Вайоминг"
+            Models.Landmark landmark1 = new Models.Landmark 
+            {
+                name = "Upper Geyser Basin",
+                rating = 5,
+                photos = new List<Models.Photo> { photo1, photo2 },
+             };
+            Models.Landmark landmark2 = new Models.Landmark
+            {
+                name = "Большой призматический источник",
+                rating = 4.9M,
+                photos = new List<Models.Photo> { photo3 },
+            };
+            //direction2 - Пунта-Кана, Доминикана
+            Models.Landmark landmark3 = new Models.Landmark
+            {
+                name = "Лагуна Азул",
+                rating = 4.7M,
+                photos = new List<Models.Photo> { photo5, photo4 },
+            };
+            //direction3 Остров Мауи, Гавайи
+            Models.Landmark landmark4 = new Models.Landmark
+            {
+                name = "Hana Highway - Road to Hana",
+                rating = 4.5M,
+                photos = new List<Models.Photo> { photo6 },
+            };
+            //direction 4 Орландо, Флорида
+            Models.Landmark landmark5 = new Models.Landmark
+            {
+                name = "Magic Kingdom",
+                rating = 4.3M,
+                photos = new List<Models.Photo> { photo7 },
+            };
+            Models.Landmark landmark6 = new Models.Landmark
+            {
+                name = "Universal Studios Florida",
+                rating = 4.4M,
+                photos = new List<Models.Photo> { photo8, photo9 },
+            };
+            //direction5 Седона, Аризона
+            Models.Landmark landmark7 = new Models.Landmark
+            {
+                name = "Soldier Pass",
+                rating = 4.4M,
+                photos = new List<Models.Photo> { photo10},
+            };
+            Models.Landmark landmark8 = new Models.Landmark
+            {
+                name = "Devil's Bridge Trail",
+                rating = 4,
+                photos = new List<Models.Photo> { photo11 },
+            };
+            //direction 6 Канкун, Мексика
+            Models.Landmark landmark9 = new Models.Landmark
+            {
+                name = "Mayan Museum of Cancun",
+                rating = 4.2M,
+                photos = new List<Models.Photo> { photo12 },
+            };
+            Models.Landmark landmark10 = new Models.Landmark
+            {
+                name = "Руины Эль-Рей",
+                rating = 4,
+                photos = new List<Models.Photo> { photo13, photo14},
+            };
+            Models.Landmark landmark11 = new Models.Landmark
+            {
+                name = "Пляж Playa Delfines",
+                rating = 4.4M,
+                photos = new List<Models.Photo> { photo15 },
+            };
+            //direction 7 Нью-Йорк, Нью-Йорк
+            Models.Landmark landmark12 = new Models.Landmark
+            {
+                name = "Empire state building",
+                rating = 3.9,
+                photos = new List<Models.Photo> { photo16},
+            };
+                Models.Landmark landmark13 = new Models.Landmark
+                {
+                    name = "Бруклинский мост",
+                    rating = 4.3M,
+                    photos = new List<Models.Photo> { photo17 },
+                };
+            Models.Landmark landmark14 = new Models.Landmark
+            {
+                name = "Центральный парк",
+                rating = 4.4M,
+                photos = new List<Models.Photo> { photo18 },
+            };
+            //direction 8 Лас-Вегас, Невада
+            Models.Landmark landmark15 = new Models.Landmark
+            {
+                name = "The Strip",
+                rating = 4.2M,
+                photos = new List<Models.Photo> { photo19 },
+            };
+            Models.Landmark landmark16 = new Models.Landmark
+            {
+                name = "Bellagio Conservatory & Botanical Garden",
+                rating = 4.3M,
+                photos = new List<Models.Photo> { photo20, photo21 },
+            };
+            //direction 9 Лондон, UK
+            Models.Landmark landmark17 = new Models.Landmark
+            {
+                name = "Лондонский Тауэр",
+                rating = 4.4M,
+                photos = new List<Models.Photo> { photo22, photo23 },
+            };
+            Models.Landmark landmark18 = new Models.Landmark
+            {
+                name = "Британский музей",
+                rating = 4.5M,
+                photos = new List<Models.Photo> { photo24 },
+            };
+
+            dataBase.landmarks.AddRange(landmark1, landmark2, landmark3, landmark4, landmark5, landmark6, landmark7, landmark8, landmark9, landmark10, landmark11, landmark12, landmark13, landmark14, landmark15, landmark16, landmark17, landmark18);
+             
+            dataBase.SaveChanges();
+        }
+
         public void createDirection()
         {
             Models.Photo photo1 = new Models.Photo { image = getByteImage(@"wwwroot\img\yellowstone-national.jpg"), name = @"img\yellowstone-national.jpg" };
@@ -69,7 +220,7 @@ namespace Project.Data
             Models.Photo photo8 = new Models.Photo { image = getByteImage(@"wwwroot\img\las-vegas.jpg"), name = @"img\las-vegas.jpg" };
             Models.Photo photo9 = new Models.Photo { image = getByteImage(@"wwwroot\img\london.jpg"), name = @"img\london.jpg" };
             dataBase.photos.AddRange(photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8, photo9);
-
+            
             Models.Direction direction1 = new Models.Direction { 
                 mainPhoto = photo1, 
                 name = "Йеллоустонский национальный парк, Вайоминг",
